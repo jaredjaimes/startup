2e2c9d4fb97d7463695f064cb78dbf5c217fc538
Jared Jaimes <jj.jaredjaimes@gmail.com>
2024-11-16 12:45:27 -0700

    hjkl;[
    ]

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,43 @@
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Avatar } from './avatar/avatar';
+import { Rankings } from './rankings/rankings';
+import { Skills } from './skills/skills';
+
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+
+    <BrowserRouter>
+        <div>
+
+            <header>
+                <h1>Olaga.io<sup>&reg;</sup></h1>
+                <nav>
+                    <menu>
+                        <button><NavLink className= "nav-button" to="index.html">Home</a></button>
+                        <button><NavLink className= "nav-button" to="avatar.html">Avatar</a></button>
+                        <button><NavLink className= "nav-button" to="skills.html">Skills</a></button>
+                        <button><NavLink className= "nav-button" to="rankings.html">Rankings</a></button>
+                    </menu>
+                </nav>
+                <hr />
+            </header>
+
+                <main> App componenets go here</main>
+
+            <footer>
+                <hr />
+                <span className="text-reset">Jared Jaimes</span>
+                <br />
+                <NavLink to="https://github.com/jaredjaimes/startup.git">GitHub</a>
+            </footer>
+
+        </div>
+    </BrowserRouter>
+    
+  );
 }
