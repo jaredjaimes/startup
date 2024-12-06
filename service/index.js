79eedfde89b515adca6e89f99e3092f0916a59d5
Jared Jaimes <jj.jaredjaimes@gmail.com>
2024-12-05 21:11:40 -0700

    Added login, create and delete end points

--- service/index.js
+++ service/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const uuid = require('uuid');
-const cors = require('cors');
 const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
-// Middleware
-app.use(express.json());
-app.use(cors());
 
 // In- memory storageok, wha
 let users = {};
@@ -15,11 +11,56 @@ let skills = [];
 let tasks = [];
 let scores = [];
 
-// Avatar Endpoints
-app.get('*', (_req, res) => {
-  res.send({ msg: 'Simon service' });
+// Middleware
+app.use(express.json());
+
+// Serves up front-end static content hosting.
+//  The React app and backend are hosted on the same server, simplifying deployment and allowing seamless integration between the frontend and backend.
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+    const user = users[req.body.email];
+    if (user) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
+      users[user.email] = user;
+  
+      res.send({ token: user.token });
+    }
 });
 
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = users[req.body.email];
+    if (user) {
+      if (req.body.password === user.password) {
+        user.token = uuid.v4();
+        res.send({ token: user.token });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', (req, res) => {
+    const user = Object.values(users).find((u) => u.token === req.body.token);
+    if (user) {
+      delete user.token;
+    }
+    res.status(204).end();
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+  
 // Start server
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
