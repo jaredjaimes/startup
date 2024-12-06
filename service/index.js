21a732923214afb8b0079171590f4557e4c434d7
Jared Jaimes <jj.jaredjaimes@gmail.com>
2024-12-05 20:09:28 -0700

    added endpoints

--- service/index.js
+++ service/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const uuid = require('uuid');
+const cors = require('cors');
 const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// In- memory storageok, wha
+let users = {};
+let skills = [];
+let tasks = [];
+let scores = [];
+
+// Avatar Endpoints
 app.get('*', (_req, res) => {
   res.send({ msg: 'Simon service' });
 });
 
+// Start server
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
