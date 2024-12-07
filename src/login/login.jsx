55d8c1677a33e9692229c0b455c56e55790e1409
Jared Jaimes <jj.jaredjaimes@gmail.com>
2024-12-07 02:08:25 -0700

    installed mongodb

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -5,6 +5,21 @@ import { Authenticated } from './authenticated';
 import { AuthState } from './authState';
 
 export function Login({ userName, authState, onAuthChange }) {
+  const [quote, setQuote] = React.useState('Loading...');
+  const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
+
+  React.useEffect(() => {
+    const random = Math.floor(Math.random() * 1000);
+
+    fetch('https://quote.cs260.click')
+    .then((response) => response.json())
+    .then((data) => {
+      setQuote(data.quote);
+      setQuoteAuthor(data.author);
+    })
+    .catch();
+}, []);
+
   return (
     <main className='container-fluid bg-secondary text-center'>
       <div>
@@ -21,7 +36,12 @@ export function Login({ userName, authState, onAuthChange }) {
           />
         )}
       </div>
+      <div className='quote-box bg-light text-dark'>
+          <p className='quote'>{quote}</p>
+          <p className='author'>{quoteAuthor}</p>
+        </div>
     </main>
   );
+  
 }
 
