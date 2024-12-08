0c499fccd1a2c13695d11d40a51669aff9254d90
Jared Jaimes <jj.jaredjaimes@gmail.com>
2024-12-07 17:50:04 -0700

    fixed the url

--- service/database.js
+++ service/database.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
-const url = `mongodb+srv://jaredjaimesstudent:12345@hostname":cluster0.mzmnu.mongodb.net`;
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('simon');
 const userCollection = db.collection('user');
