a37ff4b1de6775f743b4992f0539767f117067e8
Jared Jaimes <jj.jaredjaimes@gmail.com>
2024-12-11 16:30:20 -0700

    Updated users.jsx

---
+++ src/skills/users.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { TaskEvent , TaskNotifier } from './taskNotifier';
+import './users.css';
+
+export function Users(props) {
+    const userName = props.userName;
+
+    const [events, setEvent] = React.useState([]);
+}
