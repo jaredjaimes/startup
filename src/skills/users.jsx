e400712ddd0672ad66b74619832553a17ab043c7
Jared Jaimes <jj.jaredjaimes@gmail.com>
2024-12-11 16:33:42 -0700

    Updated users.jsx

--- src/skills/users.jsx
+++ src/skills/users.jsx
@@ -7,4 +7,20 @@ export function Users(props) {
     const userName = props.userName;
 
     const [events, setEvent] = React.useState([]);
+
+    React.useEffect(() => {
+        TaskNotifier.addHandler(handleTaskEvent);
+    
+        return () => {
+          TaskNotifier.removeHandler(handleTaskEvent);
+        };
+      });
+
+    function handleTaskEvent(event) {
+        setEvent([...events, event]);
+    }
+
+    function createMessageArray() {
+        //mock code
+    }
 }
